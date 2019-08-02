4ada60bc2eb807dce2f5e1625cc39e7cd4aa567b
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2019-08-02 14:34:11 +0700

    updated main function

--- src/main/java/com/journaldev/sparkdemo/App.java
+++ src/main/java/com/journaldev/sparkdemo/App.java
@@ -9,8 +9,10 @@ import scala.Tuple2;
 import java.util.Arrays;
 
 /**
- * Hello world!
- *
+ * 
+ * SENTIMENT ANALYSIS
+ * Application will read a file then execute word segmentation
+ * 
  */
 public class App 
 {
@@ -21,18 +23,29 @@ public class App
 		
 		JavaRDD<String> inputFile = sparkContext.textFile(fileName);
 		JavaRDD<String> wordsFromFile = inputFile.flatMap(content -> Arrays.asList(content.split(" ")));
-		JavaPairRDD countData = wordsFromFile.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) -> (int) x + (int) y);
+		JavaPairRDD segmentData = wordsFromFile.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) -> (int) x + (int) y);
 		
-		countData.saveAsTextFile("CountData");
+		segmentData.saveAsTextFile("SegmentData");
     }
 	
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
-    	if (args.length == 0) {
-            System.out.println("No files provided.");
-            System.exit(0);
-        }
-        wordCount(args[0]);
+//    	number of input files
+    	int inputFilesLenght = 10;
+  
+//    	checking if there is no input file then exit app
+    	if (inputFilesLenght <= 0) {
+    		System.out.println("No files provided.");
+    		System.exit(0);
+    	}
+   
+//    	create an array to get file name
+    	String[] fileName = new String[inputFilesLenght];
+ 
+//    	set data for file name elements
+    	fileName[0] = "input.txt";
+  
+//    	execute calculation
+    	wordCount(fileName[0]);
     }
 }
