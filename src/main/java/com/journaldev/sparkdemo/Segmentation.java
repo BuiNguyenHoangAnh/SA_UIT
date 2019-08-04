9b4916054a7a54ca6edbe6a86ed0c8cbb8951149
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2019-08-04 23:20:44 +0700

    daily commit

--- src/main/java/com/journaldev/sparkdemo/Segmentation.java
+++ src/main/java/com/journaldev/sparkdemo/Segmentation.java
@@ -1,6 +1,7 @@
 package com.journaldev.sparkdemo;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.cli.Options;
 import org.apache.spark.SparkConf;
@@ -10,28 +11,21 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import scala.Tuple2;
 
-import vn.vitk.tok.Tokenizer;
+import vn.vitk.tok.*;
 
 public class Segmentation {
 	@SuppressWarnings({ "resource", "rawtypes", "unchecked" })
 	public void wordSegmentation(String fileName) {
+		SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("Word Segmentation");
 //		String dataFolder = "/export/dat/tok";
 		String dataFolder = "/home/buinguyenhoanganh/Desktop/vn.vitk/dat/tok";
-		String master = "local[*]";
+//		String master = "local[*]";
+		String master = sparkConf.get("spark.master");	
 		String inputFileName = fileName;
 		String outputDirectory = "SegmentData";
 
-//		SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JD Word Counter");
-//		JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-		
-//		JavaRDD<String> inputFile = sparkContext.textFile(fileName);
-//		JavaRDD<String> wordsFromFile = inputFile.flatMap(content -> Arrays.asList(content.split(" ")));
-//		JavaPairRDD segmentData = wordsFromFile.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) -> (int) x + (int) y);
 		Tokenizer tokenizer = null;
 		tokenizer = new Tokenizer(master, dataFolder + "/lexicon.xml", dataFolder + "/regexp.txt", dataFolder + "/whitespace.model", true);
-//		JavaRDD<String> segmentData = tokenizer.tokenize(wordsFromFile);
 		tokenizer.tokenize(inputFileName, outputDirectory);
-		
-//		segmentData.saveAsTextFile("SegmentData");
     }
 }
