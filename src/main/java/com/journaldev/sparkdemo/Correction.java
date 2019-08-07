567bafabffeb91f2d6e2fcf588d02a4c71dca1c3
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2019-08-08 06:57:19 +0700

    add template to remove stop words

--- src/main/java/com/journaldev/sparkdemo/Correction.java
+++ src/main/java/com/journaldev/sparkdemo/Correction.java
@@ -1,5 +1,25 @@
 package com.journaldev.sparkdemo;
 
 public class Correction {
-
+	/*
+	 * loai bo stop word
+	 */
+	// read a file
+	
+	// read each line in file and push words into an array
+	
+	// check if word was a stop word
+	
+	/*
+	 * xu li tu viet tat
+	 */
+	
+	/*
+	 * xu li tu sai chinh ta
+	 */
+	 
+	/*
+	 * xu li tieng long/ vung mien
+	 */
+	
 }
